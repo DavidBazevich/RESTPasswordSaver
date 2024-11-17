afa977b32a0dad251a8a26cc18e1284612efc1e5
David <david.bazevich@yandex.ru>
2024-11-18 00:54:53 +0300

    add sample test

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+
+
+public class Test {
+
+    @org.junit.Test
+    public void test1(){
+        int a = 1;
+        int b = 1;
+        Assert.assertEquals(a,b);
+    }
+
+}
